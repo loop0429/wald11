992ee9c39bc06e609be63b58c8f38a07464df0f0
永岡史哉 <model.f.n@gmail.com>
2014-12-12 17:04:37 +0900

    ひながた

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+<meta charset="UTF-8">
+<title>Wald11 Schedule</title>
+<style>
+a {
+	color: #222;
+	text-decoration: none;
+}
+
+a:hover {
+	text-decoration: underline;
+}
+
+.sat a {
+	color: #00f;
+}
+
+.sun a {
+	color: #f00;
+}
+</style>
+</head>
+<body>
+<?php
+	//作法不明だけど、とりあえずここに関数を貯める
+
+	//二つ文字返す
+	function getNum($val, $pos) {
+		return substr($val, $pos, 2);
+	}
+?>
+<ul>
+<?php
+	//とりあえず1週間分やっとけみたいな
+	for($i = 0; $i < 7; $i++) {
+		$date = date('ymd', strtotime('+ ' . $i . ' days'));
+		$month = getNum($date, 2);
+		$day = getNum($date, 4);
+		$str = $month . '/' . $day;
+		$week = date('w', strtotime('+ ' . $i . ' days'));
+		$class = '';
+
+		//土曜・日曜の判定
+		//Todo:祝日の判定もしたい。
+		if($week === '0') {
+			$class .= ' class="sun"';
+		} else if($week === '6') {
+			$class .= ' class="sat"';
+		}
+?>
+<li<?php echo $class; ?>><a href="http://wald11.com/schedule/index.php?d=<?php echo $date; ?>"><?php echo $str; ?></a></li>
+<?php
+	}
+?>
+</ul>
+</body>
+</html>
