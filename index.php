b365a1d47abdf605872e73920472e4ba1e972034
Fumiya Nagaoka <nagaoka@mix-net.co.jp>
2014-12-13 18:00:15 +0900

    祝日判定をつけた
    毎日データを読みに言っているので、1週間分まるまる取得するようにする

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+	require_once('constants.php');
+?>
 <!DOCTYPE html>
 <html lang="jp">
 <head>
@@ -8,11 +11,44 @@
 </head>
 <body>
 <?php
-	//作法不明だけど、とりあえずここに関数を貯める
-
-	//二つ文字返す
+	/**
+	 * 二文字返す
+	 */
 	function getNum($val, $pos) {
 		return substr($val, $pos, 2);
+	}
+
+	/**
+	 * 祝日かどうかの判定を行う
+	 */
+	function checkHoliday($m, $d) {
+		//日本の祝日@googleカレンダー
+		$calendar_id = urlencode('ja.japanese#holiday@group.v.calendar.google.com');
+		$year = date('Y');
+		$start = sprintf("%04d-{$m}-{$d}T00:00:00Z", $year); //検索開始日時
+		$finish = sprintf("%04d-01-01T00:00:00Z", $year + 1); //検索終了日時
+
+		$url = "https://www.googleapis.com/calendar/v3/calendars/{$calendar_id}/events?key=" . API_KEY . "&timeMin={$start}&timeMax={$finish}&maxResults=1&singleEvents=true";
+
+		$result = file_get_contents($url);
+		$json = json_decode($result);
+		
+		if(!empty($json)) {
+			$item = $json->items;
+
+			//年内に祝日がなくなると$itemsには何もなくなるため
+			if(!empty($item)) {
+				$target = $item[0]->start->date;
+				$today = "{$year}-{$m}-{$d}";
+
+				//今日が祝日か否か判定
+				return ($target === $today) ? true : false;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
 	}
 ?>
 <ul>
@@ -25,10 +61,10 @@
 		$str = $month . '/' . $day;
 		$week = date('w', strtotime('+ ' . $i . ' days'));
 		$class = '';
+		$is_holiday = checkHoliday($month, $day);
 
 		//土曜・日曜の判定
-		//Todo:祝日の判定もしたい。
-		if($week === '0') {
+		if($week === '0' || $is_holiday) {
 			$class .= ' class="sun"';
 		} else if($week === '6') {
 			$class .= ' class="sat"';
