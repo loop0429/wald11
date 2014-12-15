0ee05484935de79381b224149c537889470e8a3a
永岡史哉 <model.f.n@gmail.com>
2014-12-15 18:16:02 +0900

    土日祝のカラーリング機能を作成

--- index.php
+++ index.php
@@ -19,58 +19,125 @@
 	}
 
 	/**
-	 * 祝日かどうかの判定を行う
+	 * 土曜・日曜・祝日用のclass
 	 */
-	function checkHoliday($m, $d) {
-		//日本の祝日@googleカレンダー
-		$calendar_id = urlencode('ja.japanese#holiday@group.v.calendar.google.com');
+	function coloring($w, $h) {
+		$color = '';
+		if($w === 0 || $h) {
+			$color .= ' class="sun"';
+		} else if($w === 6) {
+			$color .= ' class="sat"';
+		}
+
+		return $color;
+	}
+
+	/**
+	 * liタグを作成して返す
+	 */
+	function createLi($ow, $hl) {
+		$val = explode('-', $ow[0]);
+		$year = $ow[2];
+		$month = $val[0];
+		$day = $val[1];
+		$week = intval($ow[1]);
+		$text = $month . '/' . $day;
+		$class = coloring($week, $hl);
+		$wald11 = 'http://wald11.com/schedule/index.php';
+		$param = '?d=';
+
+		$li = '<li' . $class . '><a href="' . $wald11 . $param . $year . $month . $day . '">' . $text . '</a></li>';
+
+		return $li;
+	}
+
+	/**
+	 * 祝日かどうかの配列を返す
+	 */
+	function getHolidayList($ow) {
+		$calendar_id = urlencode('ja.japanese#holiday@group.v.calendar.google.com'); //日本の祝日@google
+
 		$year = date('Y');
-		$start = sprintf("%04d-{$m}-{$d}T00:00:00Z", $year); //検索開始日時
-		$finish = sprintf("%04d-01-01T00:00:00Z", $year + 1); //検索終了日時
+		
+		//今日を作成
+		$today = explode('-', $ow[0][0]);
+		$month = $today[0];
+		$day = $today[1];
 
-		$url = "https://www.googleapis.com/calendar/v3/calendars/{$calendar_id}/events?key=" . API_KEY . "&timeMin={$start}&timeMax={$finish}&maxResults=1&singleEvents=true";
+		//一週間分
+		$len = count($ow);
 
+		$start = sprintf("%04d-{$month}-{$day}T00:00:00Z", $year); //検索開始日時
+		$finish = sprintf("%04d-01-01T00:00:00Z", $year + 2); //検索終了日時。一応2年先まで。
+
+		//googleマップへ取得しに行く
+		$url = "https://www.googleapis.com/calendar/v3/calendars/{$calendar_id}/events?key=" . API_KEY . "&timeMin={$start}&timeMax={$finish}&maxResults={$len}&orderBy=startTime&singleEvents=true";
 		$result = file_get_contents($url);
 		$json = json_decode($result);
-		
+
+		//すべて平日と仮定しておく
+		$holiday_list = array(0, 0, 0, 0, 0, 0, 0);
+
 		if(!empty($json)) {
+			//直近7件の祝日リスト
 			$item = $json->items;
-
-			//年内に祝日がなくなると$itemsには何もなくなるため
 			if(!empty($item)) {
-				$target = $item[0]->start->date;
-				$today = "{$year}-{$m}-{$d}";
+				//$one_weekごとに比較する
+				for($i = 0; $i < $len; $i++) {
+					$val = explode('-', $ow[$i][0]);
+					$m = $val[0];
+					$d = $val[1];
+					//年越し
+					if($m === '01' && $d === '01') {
+						$year++;
+					}
+					$s = $year . '-' . $ow[$i][0];
+					$tmp = false;
 
-				//今日が祝日か否か判定
-				return ($target === $today) ? true : false;
-			} else {
-				return false;
+					for($j = 0; $j < count($item); $j++) {
+						$t = $item[$j]->start->date;
+
+						if($t === $s) {
+							$tmp = true;
+						}
+					}
+					if($tmp) {
+						$holiday_list[$i] = 1;
+					}
+				}
 			}
-		} else {
-			return false;
 		}
+		return $holiday_list;
+	}
+
+	/**
+	 * 日付に関する情報を返す
+	 */
+	function createDate($i) {
+		$date = date('ymdw', strtotime('+ ' . $i . ' days'));
+		$year = getNum($date, 0);
+		$month = getNum($date, 2);
+		$day = getNum($date, 4);
+		$week = getNum($date, 6);
+
+		return array($month . '-' . $day, $week, $year);
 	}
 ?>
 <ul>
 <?php
-	//とりあえず1週間分やっとけみたいな
-	for($i = 0; $i < 7; $i++) {
-		$date = date('ymd', strtotime('+ ' . $i . ' days'));
-		$month = getNum($date, 2);
-		$day = getNum($date, 4);
-		$str = $month . '/' . $day;
-		$week = date('w', strtotime('+ ' . $i . ' days'));
-		$class = '';
-		$is_holiday = checkHoliday($month, $day);
+	$one_week = array();
 
-		//土曜・日曜の判定
-		if($week === '0' || $is_holiday) {
-			$class .= ' class="sun"';
-		} else if($week === '6') {
-			$class .= ' class="sat"';
-		}
+	for($i = 0; $i < 7; $i++) {
+		$one_week[$i] = createDate($i);
+	}
+
+	$holiday_list = getHolidayList($one_week);
+	$len = count($one_week);
+
+	for($i = 0; $i < $len; $i++) {
+		$li = createLi($one_week[$i], $holiday_list[$i]);
 ?>
-<li<?php echo $class; ?>><a href="http://wald11.com/schedule/index.php?d=<?php echo $date; ?>"><?php echo $str; ?></a></li>
+<?php echo $li; ?>
 <?php
 	}
 ?>
